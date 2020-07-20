62f68f8a3acc3b11930994e874981bdf615feabf
Kosi Akporji <kosiazom@gmail.com>
2020-07-19 20:38:08 -0400

    made POST request, added listeneron create form

--- js/index.js
+++ js/index.js
@@ -23,48 +23,68 @@ document.addEventListener('DOMContentLoaded', function(e) {
     let monsterCollection = document.getElementById('monster-container')
      let div = document.createElement('div')
      div.className = 'monster'
-     div.id = `${monster.id}`
-
+     div.id = monster.id
      div.innerHTML = `
  <h2> Name: ${monster.name}</h2>
  <h4>Age: ${monster.age}</h4>
  <p>${monster.description}</p>
  `
      monsterCollection.append(div)
+    
  }
  ///////Above takes care of fetchin the data from the api
 
  function makeMonsterForm(){ 
-    let monsterForm = document.getElementById('create-monster')
+    let monsterFormContainer = document.getElementById('create-monster')
     let form = document.createElement('form')
     form.id = "monster-form"
     form.innerHTML = `
-    <input id="name" placeholder="name..."
-    <input id="age" placeholder="age..."
-    <input id= "description placeholder="description..."
+    <input id="name" placeholder="name...">
+    <input id="age" placeholder="age...">
+    <input id= "description" placeholder="description...">
+    <button>Create</button>
     ` 
-    monsterForm.append(form)
-    console.log(monsterForm)
+    monsterFormContainer.appendChild(form)
+    console.log(form)
  }
+ makeMonsterForm() //for the forms dont forget to call the function
+
  
-
-// function postMonster(newMonster){
+ 
+ function postMonster(newMonsterObject){
+     
+     fetch(POST_URL, { //makesure you use the POST_URL Variable
+         method: "POST",
+         headers: 
+         {
+             "content-type": 'application/json',
+             "accept": "application/json"
+            },
+            body: JSON.stringify(newMonsterObject)//pass in same variable that you passed in the post!
+        })
+        .then(resp => resp.json()) //another promise being returned
+        .then(newMonster => {
+            return renderAMonster(newMonster);
+        })// when you ger the new monster back then you want to be sure to render it
+    }
     
-//     fetch(url, {
-//         method: "POST",
-//         headers: 
-//         {
-//             "content-type": 'application/json',
-//             "accept": "application/json"
-//         },
-//         body: JSON.stringify(Monster)
-//     })
-//     .then(resp => resp.json()) //another promise being returned
-//     .then(newMonster => {
-//         return renderAMonster(newMonster);
-//     })// when you ger the new monster back then you want to be sure to render it
-// }
+    //add a submit eventlistener to the form 
+    const newMonsterForm = document.getElementById('monster-form') //find a variable for monster form
 
+    newMonsterForm.addEventListener('submit', function(e){ //listens for a submit
+        e.preventDefault()
+        //creater the new monster form here by grabbing the forms values its best you set that to a variable so you 
+        //pass it into the postMonster function and call it back! DOnt forget to reset the form
+                const newMonster = {
+                name: newMonsterForm.name.value,
+                age:  newMonsterForm.age.value,
+                description: newMonsterForm.description.value
+            }
+            //this information is what will make the post fetch, you can pass in the URL
+     postMonster(newMonster) //call it in the event listerner
+     //make sure you reset the newMonsterForm
+     newMonsterForm.reset() //call it after the form has been posted
+    })
 
 
 
